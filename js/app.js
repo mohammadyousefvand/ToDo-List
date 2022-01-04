1ebae9390923f2d6f4b398b0c4b9f934e133ce61
pc <www.mmhmdmm83@gmail.com>
2022-01-04 20:24:21 +0330

    add pallete color and save theme in localstorage

--- js/app.js
+++ js/app.js
@@ -2,23 +2,27 @@
 
 let $ = document
 
-let inputNewTodo = $.querySelector('.input-add-new-todo');
-let addNewTodo = $.querySelector('.add-todo');
-let close = $.querySelector('.close');
-let modal = $.querySelector('.modal');
-let downMassage = $.querySelector('.down-masage')
-let deleteMassage = $.querySelector('.delete')
-let image = $.querySelector('img');
-let textNoTodo = $.querySelector('.no-todo-text');
-let boxTodoList = $.querySelector('.list-todo');
-let inputmain = $.querySelector('.input-main');
-let submit = $.querySelector('.submit');
+const inputNewTodo = $.querySelector('.input-add-new-todo');
+const addNewTodo = $.querySelector('.add-todo');
+const close = $.querySelector('.close');
+const modal = $.querySelector('.modal');
+const downMassage = $.querySelector('.down-masage')
+const deleteMassage = $.querySelector('.delete')
+const image = $.querySelector('.cover');
+const textNoTodo = $.querySelector('.no-todo-text');
+const boxTodoList = $.querySelector('.list-todo');
+const inputmain = $.querySelector('.input-main');
+const submit = $.querySelector('.submit');
+const themeImage = $.querySelector('.theme')
+const palletColor = $.querySelector('.pallet-theme')
+const closePallete = $.querySelector('.close-pallet')
+const buttonColor = $.querySelectorAll('.btn')
+
 
 // ---- function ----
 
 let allCover = [
     'images/cover1.svg',
-    'images/cover2.svg',
     'images/cover3.svg',
     'images/cover4.svg',
     'images/cover5.svg',
@@ -89,18 +93,17 @@ function addNewTodoItem(newTodoValue) {
             newtextItem.style.cssText = 'color : #000;'
             newIconItemDoIt.style.color = '#009918'
             newIconItemClose.style.color = '#ff4800'
-            
+
             isDo = true
         }
     })
 }
 
-
-
 // ------ Event ------
 
 addNewTodo.addEventListener('click', function () {
     inputNewTodo.classList.toggle('active')
+    inputmain.focus()
 });
 
 close.addEventListener('click', function () {
@@ -153,3 +156,30 @@ inputmain.addEventListener('keydown', function (event) {
         }
     }
 });
+
+function openPalleteTheme() {
+    palletColor.classList.add('show-pallet')
+}
+
+function closePalleteColor() {
+    palletColor.classList.remove('show-pallet')
+}
+
+buttonColor.forEach(function (item) {
+    item.addEventListener('click', function (event) {
+        let choseColor = event.target.dataset.color
+
+        document.documentElement.style.setProperty('--main-color', choseColor)
+        localStorage.setItem('theme', choseColor)
+    })
+})
+
+function loadThemeFromLocalStorage() {
+    let themeLoad = localStorage.getItem('theme')
+
+    document.documentElement.style.setProperty('--main-color', themeLoad)
+}
+
+themeImage.addEventListener('click', openPalleteTheme)
+closePallete.addEventListener('click', closePalleteColor)
+window.addEventListener('load', loadThemeFromLocalStorage)
